90dff166482e12a4c59906ed82fa3d73b8e63b8d
strahinjaa22 <strahinja.raicevic@amplitudo.me>
2022-09-20 14:16:39 +0200

    Third task

---
+++ src/MathCalculations.java
@@ -0,0 +1,19 @@
+public class MathCalculations {
+    public static void main(String[] args) {
+
+        double a = 2.8;
+        double b = 3.1;
+        double c = 6.0;
+        System.out.println(a+b);
+        System.out.println(Math.abs(a));
+        System.out.println(Math.round(a));
+        System.out.println(Math.ceil(a));
+        System.out.println(Math.floor(a));
+        System.out.println(Math.exp(a));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.pow((Math.pow(2,c)-2),c));
+
+
+    }
+}

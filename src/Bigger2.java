7ef9a53c9d7dede66291c57a0b4f1cbf49659167
strahinjaa22 <strahinja.raicevic@amplitudo.me>
2022-09-21 09:56:21 +0200

    Eight task

---
+++ src/Bigger2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Bigger2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert first number:");
+        int x = input.nextInt();
+        System.out.println("Insert second number:");
+        int y = input.nextInt();
+
+        if (x>y) {
+            System.out.println("Bigger number is: " + x);
+        } else if (x<y){
+            System.out.println("Bigger number is: " + y);
+        } else {
+            System.out.println("Numbers are equal!");
+        }
+    }
+}

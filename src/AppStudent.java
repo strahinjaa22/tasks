279a56398006e97309f146f356281f612e22bf15
strahinjaa22 <strahinja.raicevic@amplitudo.me>
2022-10-04 12:22:49 +0200

    Classes and objects

---
+++ src/AppStudent.java
@@ -0,0 +1,7 @@
+public class AppStudent {
+    public static void main(String[] args) {
+        Student StudentCons = new Student("Strahinja", "Raicevic",28,4.5, false);
+        Student StudentCons2 = new Student("Marko", "Markovic",32,4.5,true);
+        System.out.println(StudentCons.name);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    String name;
+    String lastName;
+    int age;
+    double gpa;
+    boolean passedExam;
+
+    public Student(String name,String lastName, int age, double gpa, boolean passedExam){
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.gpa = gpa;
+        this.passedExam = passedExam;
+    }
+}
